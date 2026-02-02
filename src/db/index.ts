b90f76b0f709cbcd689147396bdbcc603c557948
matheuspiegas <matheusaugustopiegassales@gmail.com>
2026-02-02 17:50:20 -0300

    fix: ajustando erros de typescript e adicionando scritps de build e start

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import pg from 'pg'
-import { env } from '@/env.ts'
+import { env } from '@/src/env.ts'
 
 const pool = new pg.Pool({
 	connectionString: env.DATABASE_URL,
